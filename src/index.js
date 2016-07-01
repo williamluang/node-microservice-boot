071ebfc41f71078432ad061488cb0b2175ab5668
Jonathan <jmuller@dial-once.com>
2016-07-01 14:44:36 +0200

    Fix priority order in default config usage (env is not priority)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 require('./env');
 
 module.exports = function(config) {
-  config = Object.assign({}, config, {
+  config = Object.assign({
     BUGS_TOKEN: process.env.BUGSNAG_TOKEN,
     LOGS_TOKEN: process.env.LOGENTRIES_TOKEN
-  });
+  }, config);
 
   require('./bugsnag')(config.BUGS_TOKEN);
   require('./winston')(config.LOGS_TOKEN);
